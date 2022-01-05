c75dac4a0e8a665355153cccf60e3862189611dd
Samuel Riolo <samuel.riolo@googlemail.com>
2022-01-05 19:06:13 +0100

    use potentiometers and add time delay while switching batteries

--- src/main.cpp
+++ src/main.cpp
@@ -1,200 +1,230 @@
-#include <Arduino.h>          // Arduino core
-#include <FastLED.h>
-#include <PinChangeInterrupt.h>
-#include <LowPower.h>
-
-#include "configuration.h"
-#include "button.h"
+#include <Arduino.h>
+#include <TM1637TinyDisplay.h>
+#include "TimerOne.h"
+#include "LowPower.h"
+#include "animation.h"
 
 
+// Module connection pins (Digital Pins)
+#define CLK 9
+#define DIO 8
+#define VM A0
 
-const uint16_t mask_front_light = 0b0000000000111;
-const uint16_t mask_side_light =  0b0000111110000;
-const uint16_t mask_back_light =  0b1110000000000;
+#define R1 47000
+#define R2 4700
+
+#define THERSHOLD_PIN 13
+#define REFERENCE_VOLTAGE 3.35
+
+#define THERSHOLD_TIMEOUT 60 * 60 * 2
+#define MIN_SWITCH_DELAY 60 * 10
+
+#define LED_ON_PIN 6
+#define LED_OFF_PIN 7
+
+#define POT_LOW_PIN A1
+#define POT_HIGH_PIN A2
+#define LOW_RANGE 10
+#define HIGH_RANGE 16
+
+#define MEDIAN_SIZE 100
+
+#define DISPLAY_TIME_CHANGE 2*1000
+
+volatile unsigned long  countdown = 0;
+volatile unsigned long  minSwitchTime = 0;
 
 
-CRGB leds[NUM_LEDS];
-
-int selectedMode = 0;
-int intensity = 0;
-
-bool lock = false;
+TM1637TinyDisplay display(CLK, DIO);
 
 
-void applyColor(uint16_t mask, const struct CRGB& color, uint8_t lamp) {
-     for (int i = 0; i < (NUM_LEDS / NUM_LAMP); i++) {
-        int b = mask >> i;
-        if (b & 1) {
-            leds[i + ((NUM_LEDS / NUM_LAMP) * lamp)] = color;
-        }
+int sort_desc(const void *cmp1, const void *cmp2)
+{
+  int a = *((int *)cmp1);
+  int b = *((int *)cmp2);
+  return b - a;
+}
+
+int median(int value, int* index, int queue[], uint8_t size)
+{
+  if (*index < 0) {
+    for(int i = 0; i < size; i++){
+      queue[i] = value;
     }
+    (*index)++;
+  }
+
+
+  queue[*index] = value;
+
+  if (++(*index) >= size) {
+    *index = 0;
+  }
+
+  int lt[size];
+  for(int i = 0; i < size; i++){
+    lt[i] = queue[i];
+  }
+
+  int lt_length = sizeof(lt) / sizeof(lt[0]);
+  qsort(lt, lt_length, sizeof(lt[0]), sort_desc);
+  return lt[size / 2];
 }
 
-void full_mode() {
-  fill_solid(leds, NUM_LEDS, CRGB::White);
+float convertToVoltageAndRound(int sensor) {
+  float steps = (HIGH_RANGE - LOW_RANGE) / 1023.0;
+  float voltage = sensor * steps + LOW_RANGE;
+  return ((float)((int)(voltage * 10))) / 10;
 }
 
-void front_mode() {
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  applyColor(mask_front_light, CRGB::White, 0);
-  applyColor(mask_front_light, CRGB::White, 1);
+float readPotMeterLow() {
+  static int index = -1;
+  static int queue[MEDIAN_SIZE];
+  int sensor = median(analogRead(POT_LOW_PIN), &index, queue, MEDIAN_SIZE);
+  return convertToVoltageAndRound(sensor);
 }
 
-void blink_mode() {
-  uint8_t beat = (millis() % 1000) / 100;
-  if (beat == 1 || beat == 3) {
-    fill_solid(leds, NUM_LEDS, CRGB::White);
-  } else {
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
+float readPotMeterHigh() {
+  static int index = -1;
+  static int queue[MEDIAN_SIZE];
+  int sensor = median(analogRead(POT_HIGH_PIN), &index, queue, MEDIAN_SIZE);
+  return convertToVoltageAndRound(sensor);
+}
+
+void resetCountdown()
+{
+  noInterrupts();
+  countdown = THERSHOLD_TIMEOUT;
+  interrupts();
+}
+
+void relayOn()
+{
+  digitalWrite(THERSHOLD_PIN, HIGH);
+  digitalWrite(LED_ON_PIN, HIGH);
+  digitalWrite(LED_OFF_PIN, LOW);
+  resetCountdown();
+}
+
+void relayOff()
+{
+  digitalWrite(THERSHOLD_PIN, LOW);
+  digitalWrite(LED_ON_PIN, LOW);
+  digitalWrite(LED_OFF_PIN, HIGH);
+}
+
+void decrement(volatile unsigned long *value) {
+  if (*value > 0) {
+    (*value)--;
   }
 }
 
-void sinus_mode() {
-  const uint8_t f = 65536 / (4 * 1000);
-  uint16_t pos = sin16( ((millis() * f) % 65536) ) + 32768;
-  uint8_t color = 255 * (pos / 65536.0f);
-  fill_solid(leds, NUM_LEDS, CRGB( color, color, color));
+void secondTicker()
+{
+  decrement(&countdown);
+  decrement(&minSwitchTime);
+
+  if (countdown <= 0) {
+    relayOff();
+  }
 }
 
-void boat_mode() {
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  applyColor(mask_front_light, CRGB::White, 0);
-  applyColor(mask_front_light, CRGB::White, 1);
-  applyColor(mask_side_light, CRGB::Green, 0);
-  applyColor(mask_side_light, CRGB::Red, 1);
-  applyColor(mask_back_light, CRGB::White, 0);
-  applyColor(mask_back_light, CRGB::White, 1);
-}
+void setup()
+{
+  display.clear();
+  display.setBrightness(BRIGHT_1);
 
-void rainbow_mode() {
-  static uint16_t sPseudotime = 0;
-  static uint16_t sLastMillis = 0;
-  static uint16_t sHue16 = 0;
+  Timer1.initialize();
+  Timer1.attachInterrupt(secondTicker); // count every second
+
+  pinMode(THERSHOLD_PIN, OUTPUT);
+  pinMode(LED_ON_PIN, OUTPUT);
+  pinMode(LED_OFF_PIN, OUTPUT);
+
+  relayOff();
+
+  display.showAnimation(ANIMATION, FRAMES(ANIMATION), TIME_MS(50));
+
+
+  float potMeterLow;
+  float potMeterHigh;
+  for(int i = 0; i < MEDIAN_SIZE; i++){
+    potMeterLow = readPotMeterLow();
+    potMeterHigh = readPotMeterHigh();
+  }
+  
+  uint8_t arrL[] = { 0x38 };
+  display.setSegments(arrL, 1);
+  display.showNumber(potMeterLow/10, 3, 3, 1);
+  delay(DISPLAY_TIME_CHANGE);
  
-  uint8_t sat8 = beatsin88( 87, 220, 250);
-  uint8_t brightdepth = beatsin88( 341, 96, 224);
-  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
-  uint8_t msmultiplier = beatsin88(147, 23, 60);
-
-  uint16_t hue16 = sHue16;//gHue * 256;
-  uint16_t hueinc16 = beatsin88(113, 1, 3000);
-  
-  uint16_t ms = millis();
-  uint16_t deltams = ms - sLastMillis ;
-  sLastMillis  = ms;
-  sPseudotime += deltams * msmultiplier;
-  sHue16 += deltams * beatsin88( 400, 5,9);
-  uint16_t brightnesstheta16 = sPseudotime;
-  
-  for( uint16_t i = 0 ; i < NUM_LEDS; i++) {
-    hue16 += hueinc16;
-    uint8_t hue8 = hue16 / 256;
-
-    brightnesstheta16  += brightnessthetainc16;
-    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
-
-    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
-    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
-    bri8 += (255 - brightdepth);
-    
-    CRGB newcolor = CHSV( hue8, sat8, bri8);
-    
-    uint16_t pixelnumber = i;
-    pixelnumber = (NUM_LEDS-1) - pixelnumber;
-    
-    nblend( leds[pixelnumber], newcolor, 64);
-  }
+  uint8_t arrH[] = { 0x76 };
+  display.setSegments(arrH, 1);
+  display.showNumber(potMeterHigh/10, 3, 3, 1);
+  delay(DISPLAY_TIME_CHANGE);
+ 
 }
 
-void display_off() {
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-}
+void loop()
+{
+  static int index = -1;
+  static int queue[MEDIAN_SIZE];
+ 
+  int sensor = median(analogRead(VM), &index, queue, MEDIAN_SIZE);
+  float vOut = sensor * (REFERENCE_VOLTAGE / 1023.0);
+  float vIn = vOut * ((R1 + R2) / R2); 
 
-void display_starting() {
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  applyColor(mask_side_light, CRGB::Yellow, 0);
-  applyColor(mask_side_light, CRGB::Yellow, 1);
-}
 
-void display_lock() {
-  fill_solid(leds, NUM_LEDS, CRGB::Black);
-  bool beat = (millis() / 50) % 2;
-  if(beat) {
-    return;
+  static unsigned long displayLowMilis;
+  static unsigned long displayHighMilis;
+
+  float potMeterLow = readPotMeterLow();
+  float potMeterHigh = readPotMeterHigh();
+
+  static float lastPotMeterLow;
+  static float lastPotMeterHigh;
+
+
+  if (potMeterLow != lastPotMeterLow) {
+    displayLowMilis = millis();
   }
-  CRGB color;
-  if (lock) {
-    color = CRGB::Red;
-  }  else {
-    color = CRGB::Green;
+
+  if (potMeterHigh != lastPotMeterHigh) {
+    displayHighMilis = millis();
   }
-  applyColor(mask_side_light, color, 0);
-  applyColor(mask_side_light, color, 1);
-}
 
-void (*funcs[])(void) = {full_mode, front_mode, blink_mode, sinus_mode, boat_mode, rainbow_mode};
-uint8_t funcs_size = sizeof(funcs)/sizeof(funcs[0]);
+  lastPotMeterLow = potMeterLow;
+  lastPotMeterHigh = potMeterHigh;
 
-void intensityClick() {
-  if (lock) {
-    return;
-  }
-  intensity--;
-  if (intensity < 0) {
-    intensity = FADE_MAX_STEPS;
-  }
-}
+  potMeterLow = min(potMeterLow, potMeterHigh);
 
-void modeClick() {
-  if (lock) {
-    return;
-  }
-  if (intensity == 0) {
-    return;
-  }
-  selectedMode++;
-  if (!(selectedMode < funcs_size)) {
-    selectedMode = 0;
-  }
-}
-
-void longClick() {
-  if (buttonIntensity->longClickPending() && buttonMode->longClickPending()) {
-    lock = !lock;
-  }
-}
-
-void setup() {
-    buttonIntensity->attach(intensityClick, longClick);
-    buttonMode->attach(modeClick, longClick);
-   	delay(2000);
-    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-
-    display_starting();
-    FastLED.show();
-    delay(100);
-    display_off();
-    FastLED.show();
-}
-
-
-void loop() {
-  static const uint8_t step_map[FADE_MAX_STEPS + 1] = {0, 30, 100, 255}; 
-  
-  if (buttonIntensity->longClickPending() && buttonMode->longClickPending()) {
-    LEDS.setBrightness(255);
-    display_lock();
-    FastLED.show();
-  } else {
-    if (intensity == 0 && !buttonIntensity->isPressed() && !buttonMode->isPressed()) {
-      display_off();
-      FastLED.show();
-      LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  if (displayLowMilis + DISPLAY_TIME_CHANGE > millis() || displayHighMilis + DISPLAY_TIME_CHANGE > millis()) {
+    if (displayHighMilis > displayLowMilis) {
+      uint8_t arr[] = { 0x76 };
+      display.setSegments(arr, 1);
+      display.showNumber(potMeterHigh/10, 3, 3, 1);
+    } else {
+      uint8_t arr[] = { 0x38 };
+      display.setSegments(arr, 1);
+      display.showNumber(potMeterLow/10, 3, 3, 1);
     }
-
-    funcs[selectedMode]();
-
-    LEDS.setBrightness(step_map[intensity]);
-    FastLED.show();
+  } else {
+    display.showNumber(vIn);
   }
+
+  if (vIn >= potMeterHigh && minSwitchTime <= 0) {
+    relayOn();
+    minSwitchTime = MIN_SWITCH_DELAY;
+  }
+ 
+  if (vIn <= potMeterLow && minSwitchTime <= 0) {
+    relayOff();
+    minSwitchTime = MIN_SWITCH_DELAY;
+  }
+
+  LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_ON, TIMER0_ON, 
+                SPI_OFF, USART0_OFF, TWI_OFF);
+
 }
+
+
